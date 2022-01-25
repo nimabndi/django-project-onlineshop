46d1ba5f29957890ae8f91030594ee219a788f7d
nimabandi <nimabandi0935@gmail.com>
2022-01-25 01:34:18 -0800

    add category

--- shop/views.py
+++ shop/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 
-def home(request):
+def home(request, slug=None):
     products = Product.objects.filter(available=True)
-    return render(request, 'shop/home.html', {"products": products})
+    categories = Category.objects.all()
+    if slug:
+        category = get_object_or_404(Category, slug=slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/home.html', {"products": products, 'categories': categories})
 
 
 def product_detail(request, slug):
