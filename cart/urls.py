9899099dd3bc94bd3f29719787f1b99ea16f2a08
nimabandi <nimabandi0935@gmail.com>
2022-01-26 01:42:22 -0800

    add cart.py

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'cart'
 urlpatterns = [
     path('', views.detail, name='detail'),
+    path('add/<int:product_id>/', views.cart_add, name='cart_add'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def detail(request):
     return render(request, 'cart/detail.html')
+
+
+def cart_add(request):
+    pass
