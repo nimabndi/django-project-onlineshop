267da79610f8bccb19636822cc7ca2f55206ce87
nimabandi <nimabandi0935@gmail.com>
2022-01-26 00:44:48 -0800

    add detail.htm cart urls

--- O/urls.py
+++ O/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
