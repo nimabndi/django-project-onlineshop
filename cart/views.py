ea033ee0ca175a80af8a8bd45706334d057a51b9
nimabandi <nimabandi0935@gmail.com>
2022-01-26 02:41:30 -0800

    changed cart.py cartviews

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .cart import Cart
+from shop.models import Product
+from .forms import CartAddForm
+from django.views.decorators.http import require_POST
 
 
 def detail(request):
     return render(request, 'cart/detail.html')
 
-
-def cart_add(request):
-    pass
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'])
+    return redirect('cart:detail')
