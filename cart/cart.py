7deea2683d05e73e7642f372ed7c95d8d32400c5
nimabandi <nimabandi0935@gmail.com>
2022-01-29 02:00:08 -0800

    create order

--- cart/cart.py
+++ cart/cart.py
@@ -42,5 +42,9 @@ class Cart:
     def get_total_price(self):
         return sum(item['total_price'] for item in self.cart.values())
 
+    def clear(self):
+        del self.session[CART_SESSION_ID]
+        self.save()
+
     def save(self):
         self.session.modified = True
